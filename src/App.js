6216dda641e08bf087d2cf19a73ea0a54e635d32
Nick <nmcarducci@gmail.com>
2022-04-23 09:42:47 -0400

    ok

---
+++ src/App.js
@@ -0,0 +1,135 @@
+import React from "react";
+//import TwitterTweetEmbed from "./TwitterTweetEmbed";
+import { UAParser } from "ua-parser-js";
+import Cable from "./Dropwire";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    var parser = new UAParser();
+    const name = parser.getBrowser().name;
+    console.log(name);
+    this.state = {
+      trigger: false,
+      browser: name,
+      scrollTop: 0,
+      serviceCancelingImages: name.includes("Safari")
+    };
+    for (let i = 0; i < 220; i++) {
+      this["scrollImg" + i] = React.createRef();
+    }
+  }
+  componentDidMount = () => {
+    window.addEventListener("resize", this.refresh);
+    window.addEventListener("scroll", this.handleScroll);
+    this.refresh(true);
+  };
+  componentWillUnmount = () => {
+    clearTimeout(this.check);
+    clearTimeout(this.scrollTimeout);
+    clearTimeout(this.resizeTimer);
+    window.removeEventListener("resize", this.refresh);
+    window.removeEventListener("scroll", this.handleScroll);
+  };
+  handleScroll = (e) => {
+    if (!this.state.offScroll) {
+      const scrollTop = window.scrollY;
+      this.setState(
+        {
+          scrolling: true,
+          scrollTop
+        },
+        () => {
+          clearTimeout(this.scrollTimeout);
+          this.scrollTimeout = setTimeout(() => {
+            this.setState({
+              scrolling: false
+            });
+          }, 900);
+        }
+      );
+    }
+  };
+  refresh = (first) => {
+    const width =
+      (this.state.ios ? window.screen.availWidth : window.innerWidth) - 20;
+    if (first || Math.abs(this.state.lastWidth - width) > 0) {
+      clearTimeout(this.resizeTimer);
+      this.resizeTimer = setTimeout(() => {
+        this.setState({
+          lastWidth: width,
+          width,
+          availableHeight: this.state.ios
+            ? window.screen.availHeight - 20
+            : window.innerHeight
+        });
+      }, 600);
+    }
+  };
+  componentDidUpdate = (prevProps) => {
+    if (this.props.pathname !== prevProps.pathname) {
+      clearTimeout(this.check);
+      const check = () => {
+        if (this.props.pathname !== "/") {
+          this.setState({ trigger: true });
+        }
+        if (this.props.pathname === "/edu") {
+          //  window.scroll(0, this.edu.current.offsetTop);
+        }
+      };
+      check();
+      this.check = setTimeout(check, 4000);
+    }
+  };
+  render() {
+    const handleScollImgError = (e) => {
+      if (e.message) {
+        console.log(e.message);
+        this.setState({ serviceCancelingImages: true });
+      }
+    };
+    let arrayOfnumbers = 0;
+    const scrollnum = () => {
+      arrayOfnumbers = arrayOfnumbers + 1; //arrayOfnumbers[arrayOfnumbers.length - 1] + 1;
+      //arrayOfnumbers.push(num);
+      //console.log(arrayOfnumbers)
+      return arrayOfnumbers;
+    };
+    const space = " ";
+    return (
+      <div
+        style={{
+          overflow: "hidden",
+          //margin: "5px",
+          fontFamily: "arial, sans serif",
+          wordBreak: "break-word",
+          textAlign: "left",
+          width: "100%",
+          maxWidth: "600px",
+          position: "relative"
+        }}
+      >
+        {/*<Cable
+          style={{ width: "200px" }}
+          onError={handleScollImgError}
+          img={true}
+          src={
+            this.state.noyout
+              ? ""
+              : "https://www.dropbox.com/s/sqf6c2uuuf4kjzg/Screen%20Shot%202022-03-17%20at%201.39.57%20PM.png?raw=1"
+          }
+          float={"left"}
+          title="The Future of Medicine: Legislation to Encourage Innovation and Improve Oversight (Energy and Commerce Committee, Youtube) - https://youtu.be/Oiy0aFXyKzU"
+          scrolling={this.state.scrolling}
+          fwd={this["scrollImg" + scrollnum()]}
+          scrollTopAndHeight={this.state.scrollTop + window.innerHeight}
+          scrollTop={this.state.scrollTop}
+        />*/}
+        <h4>
+          <a href="https://banksquat.com">banksquat.com</a>
+        </h4>
+        Third party donee beneficiary and borrower duress
+      </div>
+    );
+  }
+}
